40f844f6cabddf0609a7363e960b64c52e51cfa7
aborgx <aborgx@users.noreply.github.com>
2026-07-17 17:15:13 +0200

    fix: update Godot path in bat files (Windows Defender flag earlier copy)

--- play_level1.bat
+++ play_level1.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "E:\Giochini\Giuseppe"
 echo Avvio Level 1...
-start "" "E:\Applicazioni\Godot_v4.7.1-stable_win64.exe\Godot_v4.7.1-stable_win64.exe" --path "E:\Giochini\Giuseppe" "res://scenes/main.tscn"
+"E:\Applicazioni\Godot\Godot_v4.7.1-stable_win64.exe" --path "E:\Giochini\Giuseppe" "res://scenes/main.tscn"
 pause
--- run_godot.bat
+++ run_godot.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d E:\Giochini\Giuseppe
-start "" "E:\Applicazioni\Godot_v4.7.1-stable_win64.exe" .
+cd /d "E:\Giochini\Giuseppe"
+start "" "E:\Applicazioni\Godot\Godot_v4.7.1-stable_win64.exe" .
 echo Godot editor avviato. Premi F5 per run.
 pause
